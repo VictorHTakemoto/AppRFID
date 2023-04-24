814e9bf7ab114dada14046fbbffd6a3ce85fb5fe
Victor Takemoto <victor_takemoto@outlook.com>
2023-04-24 09:55:41 -0300

    App RFID

---
+++ app/src/main/java/com/honeywell/rfidsimpleexample/MainActivity.java
@@ -0,0 +1,356 @@
+package com.honeywell.rfidsimpleexample;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.honeywell.rfidservice.EventListener;
+import com.honeywell.rfidservice.RfidManager;
+import com.honeywell.rfidservice.TriggerMode;
+import com.honeywell.rfidservice.rfid.RfidReader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    private String[] mPermissions = new String[]{Manifest.permission.ACCESS_COARSE_LOCATION};
+    private List<String> mRequestPermissions = new ArrayList<>();
+
+    private MyApplication mMyApplication;
+    private RfidManager mRfidMgr;
+    private BluetoothAdapter mBluetoothAdapter;
+
+    private Handler mHandler = new Handler();
+    private ProgressDialog mWaitDialog;
+    private TextView mTvInfo;
+    private Button mBtnConnect;
+    private Button mBtnCreateReader;
+    private ListView mLv;
+    private MyAdapter mAdapter;
+    private List<BtDeviceInfo> mDevices = new ArrayList();
+    private int mSelectedIdx = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mMyApplication = MyApplication.getInstance();
+        mRfidMgr = mMyApplication.rfidMgr;
+
+        setContentView(R.layout.activity_main);
+        mTvInfo = findViewById(R.id.tv_info);
+        mBtnConnect = findViewById(R.id.btn_connect);
+        mBtnCreateReader = findViewById(R.id.btn_create_reader);
+        showBtn();
+        mLv = findViewById(R.id.lv);
+        mAdapter = new MyAdapter(this, mDevices);
+        mLv.setAdapter(mAdapter);
+        mLv.setOnItemClickListener(mItemClickListener);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        requestPermissions();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mRfidMgr.addEventListener(mEventListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopScan();
+        mHandler.removeCallbacksAndMessages(null);
+        mRfidMgr.removeEventListener(mEventListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_bt, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.scan:
+                scan();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private boolean requestPermissions() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            for (int i = 0; i < mPermissions.length; i++) {
+                if (ContextCompat.checkSelfPermission(this, mPermissions[i]) != PackageManager.PERMISSION_GRANTED) {
+                    mRequestPermissions.add(mPermissions[i]);
+                }
+            }
+
+            if (mRequestPermissions.size() > 0) {
+                ActivityCompat.requestPermissions(this, mPermissions, PERMISSION_REQUEST_CODE);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void showBtn() {
+        mTvInfo.setTextColor(Color.rgb(128, 128, 128));
+
+        if (isConnected()) {
+            mTvInfo.setText(mMyApplication.macAddress + " connected.");
+            mTvInfo.setTextColor(Color.rgb(0, 128, 0));
+            mBtnConnect.setEnabled(true);
+            mBtnConnect.setText("Disconnect");
+            mBtnCreateReader.setEnabled(true);
+        } else {
+            mTvInfo.setText("Device not connected.");
+            mBtnConnect.setEnabled(mSelectedIdx != -1);
+            mBtnConnect.setText("Connect");
+            mBtnCreateReader.setEnabled(false);
+        }
+    }
+
+    private boolean isConnected() {
+        if (mRfidMgr.isConnected()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void clickBtnConn(View v) {
+        if (isConnected()) {
+            disconnect();
+        } else {
+            connect();
+        }
+    }
+
+    public void clickBtnCreateReader(View view) {
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mRfidMgr.createReader();
+            }
+        }, 1000);
+
+        mWaitDialog = ProgressDialog.show(this, null, "Creating reader...");
+    }
+
+    private void scan() {
+        if (!requestPermissions()) {
+            return;
+        }
+
+        mDevices.clear();
+        mSelectedIdx = -1;
+        mAdapter.notifyDataSetChanged();
+        mBluetoothAdapter.startLeScan(mLeScanCallback);
+
+        mWaitDialog = ProgressDialog.show(this, null, "Scanning Bluetooth devices...");
+        mWaitDialog.setCancelable(false);
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                stopScan();
+            }
+        }, 5 * 1000);
+    }
+
+    private void stopScan() {
+        mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        closeWaitDialog();
+    }
+
+    private void connect() {
+        if (mSelectedIdx == -1 || mSelectedIdx >= mDevices.size()) {
+            return;
+        }
+
+        mRfidMgr.addEventListener(mEventListener);
+        mRfidMgr.connect(mDevices.get(mSelectedIdx).dev.getAddress());
+        mWaitDialog = ProgressDialog.show(this, null, "Connecting...");
+    }
+
+    private void disconnect() {
+        mRfidMgr.disconnect();
+    }
+
+    private void closeWaitDialog() {
+        if (mWaitDialog != null) {
+            mWaitDialog.dismiss();
+            mWaitDialog = null;
+        }
+    }
+
+    private EventListener mEventListener = new EventListener() {
+        @Override
+        public void onDeviceConnected(Object o) {
+            mMyApplication.macAddress = (String) o;
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    showBtn();
+                    closeWaitDialog();
+                }
+            });
+        }
+
+        @Override
+        public void onDeviceDisconnected(Object o) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    showBtn();
+                    closeWaitDialog();
+                }
+            });
+        }
+
+        @Override
+        public void onReaderCreated(boolean b, RfidReader rfidReader) {
+            MyApplication.getInstance().mRfidReader = rfidReader;
+            Intent intent = new Intent(MainActivity.this, ReadActivity.class);
+            startActivity(intent);
+        }
+
+        @Override
+        public void onRfidTriggered(boolean b) {
+        }
+
+        @Override
+        public void onTriggerModeSwitched(TriggerMode triggerMode) {
+        }
+    };
+
+    private long mPrevListUpdateTime;
+    private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+            if (device.getName() != null && !device.getName().isEmpty()) {
+                synchronized (mDevices) {
+                    boolean newDevice = true;
+
+                    for (BtDeviceInfo info : mDevices) {
+                        if (device.getAddress().equals(info.dev.getAddress())) {
+                            newDevice = false;
+                            info.rssi = rssi;
+                        }
+                    }
+
+                    if (newDevice) {
+                        mDevices.add(new BtDeviceInfo(device, rssi));
+                    }
+
+                    long cur = System.currentTimeMillis();
+
+                    if (newDevice || cur - mPrevListUpdateTime > 500) {
+                        mPrevListUpdateTime = cur;
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mAdapter.notifyDataSetChanged();
+                            }
+                        });
+                    }
+                }
+            }
+        }
+    };
+
+    private class BtDeviceInfo {
+        BluetoothDevice dev;
+        int rssi;
+
+        private BtDeviceInfo(BluetoothDevice dev, int rssi) {
+            this.dev = dev;
+            this.rssi = rssi;
+        }
+    }
+
+    private class MyAdapter extends ArrayAdapter {
+        private Context ctx;
+
+        public MyAdapter(Context context, List ls) {
+            super(context, 0, ls);
+            ctx = context;
+        }
+
+        public View getView(int position, @Nullable View v, @NonNull ViewGroup parent) {
+            ViewHolder vh;
+
+            if (v == null) {
+                LayoutInflater inflater = LayoutInflater.from(ctx);
+                v = inflater.inflate(R.layout.list_item_bt_device, null);
+                vh = new ViewHolder();
+                vh.tvName = v.findViewById(R.id.tvName);
+                vh.tvAddr = v.findViewById(R.id.tvAddr);
+                vh.tvRssi = v.findViewById(R.id.tvRssi);
+                v.setTag(vh);
+            } else {
+                vh = (ViewHolder) v.getTag();
+            }
+
+            BtDeviceInfo item = mDevices.get(position);
+            vh.tvName.setText(item.dev.getName());
+            vh.tvAddr.setText(item.dev.getAddress());
+            vh.tvRssi.setText(String.valueOf(item.rssi));
+
+            if (position == mSelectedIdx) {
+                v.setBackgroundColor(Color.rgb(220, 220, 220));
+            } else {
+                v.setBackgroundColor(Color.argb(0, 0, 0, 0));
+            }
+
+            return v;
+        }
+
+        class ViewHolder {
+            TextView tvName;
+            TextView tvAddr;
+            TextView tvRssi;
+        }
+    }
+
+    private AdapterView.OnItemClickListener mItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            mSelectedIdx = i;
+            mAdapter.notifyDataSetChanged();
+            showBtn();
+        }
+    };
+}
---
+++ app/src/main/java/com/honeywell/rfidsimpleexample/ReadActivity.java
@@ -0,0 +1,281 @@
+package com.honeywell.rfidsimpleexample;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.honeywell.rfidservice.EventListener;
+import com.honeywell.rfidservice.RfidManager;
+import com.honeywell.rfidservice.TriggerMode;
+import com.honeywell.rfidservice.rfid.OnTagReadListener;
+import com.honeywell.rfidservice.rfid.RfidReader;
+import com.honeywell.rfidservice.rfid.RfidReaderException;
+import com.honeywell.rfidservice.rfid.TagAdditionData;
+import com.honeywell.rfidservice.rfid.TagReadData;
+import com.honeywell.rfidservice.rfid.TagReadOption;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReadActivity extends AppCompatActivity {
+    private RfidManager mRfidMgr;
+    private RfidReader mReader;
+    private List mTagDataList = new ArrayList();
+
+    private boolean mIsReadBtnClicked;
+    private Button mBtnRead;
+    private ListView mLv;
+    private ArrayAdapter mAdapter;
+    private Button mBtnWriteTag;
+    private int mSelectedIdx = -1;
+    private TextView mTagToWrite;
+    private TextView mData;
+    private ProgressDialog mWaitDialog;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mRfidMgr = MyApplication.getInstance().rfidMgr;
+        mReader = MyApplication.getInstance().mRfidReader;
+        setContentView(R.layout.activity_read);
+        mBtnRead = findViewById(R.id.btn_read);
+        mBtnWriteTag = findViewById(R.id.write_tag);
+        mLv = findViewById(R.id.lv);
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mTagDataList);
+        mLv.setAdapter(mAdapter);
+        mLv.setOnItemClickListener(mItemClickListenerTag);
+        mTagToWrite = findViewById(R.id.select_tag);
+        mData = findViewById(R.id.data_field);
+        mData.requestFocus();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mRfidMgr.addEventListener(mEventListener);
+        showBtn();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mRfidMgr.removeEventListener(mEventListener);
+        mIsReadBtnClicked = false;
+        stopRead();
+    }
+
+    private void showBtn() {
+        if (mIsReadBtnClicked) {
+            mBtnRead.setText("Stop");
+            mBtnRead.setTextColor(Color.rgb(255, 128, 0));
+        } else {
+            mBtnRead.setText("Read");
+            mBtnRead.setTextColor(Color.rgb(0, 0, 0));
+        }
+    }
+
+    //Navegação de telas, desativado para validação
+    /*public void WriteTag(View view) {
+        Intent intent = new Intent(this, WriteTag.class);
+        startActivity(intent);
+    }*/
+
+    public void clickBtnRead(View view) {
+        if (mIsReadBtnClicked) {
+            mIsReadBtnClicked = false;
+            stopRead();
+        } else {
+            mIsReadBtnClicked = true;
+            read();
+        }
+
+        showBtn();
+    }
+
+
+    private EventListener mEventListener = new EventListener() {
+        @Override
+        public void onDeviceConnected(Object o) {
+        }
+
+        @Override
+        public void onDeviceDisconnected(Object o) {
+        }
+
+        @Override
+        public void onReaderCreated(boolean b, RfidReader rfidReader) {
+        }
+
+        @Override
+        public void onRfidTriggered(boolean trigger)
+        {
+            int duration = Toast.LENGTH_LONG;
+            String checkFieldEpc = mTagToWrite.getText().toString();
+            String checkFieldData = mData.getText().toString();
+            if (mIsReadBtnClicked || !trigger)
+            {
+                mIsReadBtnClicked = false;
+                stopRead();
+
+                runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        showBtn();
+                    }
+                });
+            }
+            else
+            {
+                if (checkFieldEpc.isEmpty() || checkFieldEpc == null)
+                {
+                    read();
+                }
+                else
+                {
+                    if(checkFieldData.isEmpty() || checkFieldData == null)
+                    {
+                        Toast toast = Toast.makeText(getApplicationContext(), "Leia um codigo de barras", duration);
+                        toast.show();
+                    }
+                    else
+                    {
+                        int size = 25;
+                        char padChar = '0';
+                        int bank = 1;
+                        int startAddress = 2;
+                        String dataToWrite = padLeft(checkFieldData, size, padChar);
+                        writeTagData(checkFieldEpc, bank, startAddress, dataToWrite);
+                        //Toast toast = Toast.makeText(getApplicationContext(), dataToWrite, duration);
+                        //toast.show();
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void onTriggerModeSwitched(TriggerMode triggerMode) {
+        }
+    };
+
+    private boolean isReaderAvailable() {
+        return mReader != null && mReader.available();
+    }
+
+    private void read() {
+        if (isReaderAvailable()) {
+            mTagDataList.clear();
+            mReader.setOnTagReadListener(dataListener);
+            mReader.read(TagAdditionData.get("None"), new TagReadOption());
+        }
+    }
+
+    private void stopRead() {
+        if (isReaderAvailable()) {
+            mReader.stopRead();
+            mReader.removeOnTagReadListener(dataListener);
+        }
+    }
+
+    private OnTagReadListener dataListener = new OnTagReadListener() {
+        @Override
+        public void onTagRead(final TagReadData[] t) {
+            synchronized (mTagDataList) {
+                for (TagReadData trd : t) {
+                    String epc = trd.getEpcHexStr();
+
+                    if (!mTagDataList.contains(epc)) {
+                        mTagDataList.add(epc);
+                    }
+                }
+
+                mHandler.sendEmptyMessage(0);
+            }
+        }
+    };
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            switch (msg.what) {
+                case 0:
+                    mAdapter.notifyDataSetChanged();
+                    break;
+            }
+        }
+    };
+
+    private AdapterView.OnItemClickListener mItemClickListenerTag = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+            mSelectedIdx = position;
+            mAdapter.notifyDataSetChanged();
+            String selectedItem = (String) adapterView.getItemAtPosition(position);
+            mTagToWrite.setText(selectedItem);
+        }
+    };
+
+    public String padLeft(String str, int size, char padChar) {
+        StringBuilder padded = new StringBuilder(str);
+        while (padded.length() < size) {
+            padded.insert(0, padChar);
+        }
+        return padded.toString();
+    }
+
+    public void checkLengthToWrite(View view) {
+        int duration = Toast.LENGTH_LONG;
+        try {
+            String tagToCheck = mData.getText().toString();
+            if (tagToCheck.isEmpty() || tagToCheck == null) {
+                mWaitDialog = ProgressDialog.show(this, null, "Leia um codigo de barras");
+            } else {
+                int size = 25;
+                char padChar = '0';
+                String tagCheck = padLeft(tagToCheck, size, padChar);
+                //Desativado para validar
+           /* if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+                byte[] getBytes = tagCheck.getBytes(StandardCharsets.UTF_8);
+                String hexa = new BigInteger(1, getBytes).toString(16);
+                mWaitDialog = ProgressDialog.show(this, null, hexa);
+            }*/
+            }
+        } catch (Exception exception) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Exceção gerada: " + exception.getMessage(), duration);
+            toast.show();
+        }
+    }
+
+    private void writeTagData(String epc, int bank, int startAddr, String data) {
+        int duration = Toast.LENGTH_LONG;
+        try {
+            mReader.writeTagData(epc, bank, startAddr, null, data);
+            mTagToWrite.setText("");
+            mData.setText("");
+            Toast toast = Toast.makeText(getApplicationContext(), "Tag gravada com sucesso", duration);
+            toast.show();
+        } catch (RfidReaderException e) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Não foi posivel gravar a tag,: " + e.getMessage(), duration);
+            toast.show();
+        }
+    }
+
+}
